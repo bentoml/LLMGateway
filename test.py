95e5ae5e098a4fa892647149c6583b2d28c6c09f
Zhao Shenyang <dev@zsy.im>
2024-07-26 14:07:00 +0800

    minor fix

--- test.py
+++ test.py
@@ -4,13 +4,13 @@ from openai import OpenAI
 BASE_URL = os.environ.get("BASE_URL", "http://localhost:3000/v1")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "n/a")
 PROMPT = os.environ.get("PROMPT", "Explain superconductors like I'm five years old in 500 words")
-STREAM = True if os.environ.get("STREAM") == 1 else False
+STREAM = True if os.environ.get("STREAM") == "1" else False
 MODEL = os.environ.get("MODEL", "gpt-3.5-turbo")
 
 client = OpenAI(base_url=BASE_URL, api_key=OPENAI_API_KEY)
 
 chat_completion = client.chat.completions.create(
-    model="llama3.1",
+    model=MODEL,
     messages=[
         {
             "role": "user",
